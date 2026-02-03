097dbe59e2279f0e9890726c829e57a104923b98
Anurag Dubey <anuragdubey26202@gmail.com>
2026-02-03 23:04:01 +0530

    Implement Pydantic model and output parsing for prompts
    Added a Pydantic model for Person and integrated it with a prompt template and output parser for generating fictional character details.

--- 6.Output_Parsers/pydanticoutputparser.py
+++ 6.Output_Parsers/pydanticoutputparser.py
@@ -1,8 +1,8 @@
-from langchain_huggingface import ChatHuggingFace , HuggingFaceEndpoint
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
 from dotenv import load_dotenv
 from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import PydanticOutputParser
-from pydantic import BaseModel , Field
+from pydantic import BaseModel, Field
 
 load_dotenv()
 
@@ -14,4 +14,28 @@ llm = HuggingFaceEndpoint(
 model = ChatHuggingFace(llm = llm)
 
 class Person(BaseModel):
+    name: str = Field(description='Name of the person')
+    age: int = Field(gt=18 , description='Age of the the person')
+    city: str = Field(description='Name of the city the person belong to')
     
+parser = PydanticOutputParser(pydantic_object=Person)
+
+template = PromptTemplate(
+    template='Generate the name, age and city of a frictional {place} person\n {format_instruction}',
+    input_variables=['place'],
+    partial_variables={'format_instruction':parser.get_format_instructions()}
+)
+
+# prompt = template.invoke({'place':'india'})
+# print(prompt)
+# result = model.invoke(prompt)
+# final_result =  parser.parse(result.content)
+
+# doing the above process with the help of chaining
+
+
+chain = template | model | parser
+
+final_result = chain.invoke({'place':'sri lankan'})
+
+print(final_result)
