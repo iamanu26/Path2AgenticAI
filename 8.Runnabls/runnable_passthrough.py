85e8dc50c6b4fa718aa3c03e9dc9bae01153eeb3
Anurag Dubey <anuragdubey26202@gmail.com>
2026-02-08 21:57:37 +0530

    learning runnable passthrough

---
+++ 8.Runnabls/runnable_passthrough.py
@@ -0,0 +1,39 @@
+from langchain_huggingface import ChatHuggingFace , HuggingFaceEndpoint
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from dotenv import load_dotenv
+from langchain_core.runnables import RunnableSequence , RunnableParallel , RunnablePassthrough
+
+load_dotenv()
+
+llm = HuggingFaceEndpoint(
+    repo_id="mistralai/Mistral-7B-Instruct-v0.2",
+    task="text-generation"
+)
+
+prompt1 = PromptTemplate(
+    template='Write a joke about {topic}',
+    input_variables=['topic']
+)
+
+prompt2 = PromptTemplate(
+    template='Expalin the following jokes - {text}',
+    input_variables=['text']
+)
+
+model = ChatHuggingFace(llm = llm)
+
+parser = StrOutputParser()
+
+joke_gen_chain = RunnableSequence(prompt1 , model , parser)
+
+parallel_chain = RunnableParallel({
+    'joke':RunnablePassthrough(),
+    'explanation':RunnableSequence(prompt2 , model , parser)
+})
+
+final_chain = RunnableSequence(joke_gen_chain , parallel_chain)
+
+print(final_chain.invoke({'topic':'cricket'}))
+
+
